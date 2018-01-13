af8ed7b0cb9bc8436e21475ea45167e640a1fbb9
Bryan <btteh@hotmail.com>
2018-01-14 02:59:27 +0800

    Able to spawn random power ups

---
+++ CyberCrack/Assets/Assets/Scripts/Power/RandPowerUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandPowerUp : MonoBehaviour
+{
+    public List<GameObject> powerUpList = new List<GameObject>();
+	// Use this for initialization
+	void Start ()
+    {
+        foreach (GameObject powerUp in Resources.LoadAll("Prefabs/PowerUps"))
+            powerUpList.Add(powerUp);
+
+        SpawnRandPowerUp();
+	}
+
+    void SpawnRandPowerUp()
+    {
+        GameObject newPowerUp = Instantiate(powerUpList[Random.Range(0, powerUpList.Count)], transform.position, transform.rotation, transform.parent);
+        Destroy(gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
