2f0ba03bbceb34a9fee2f81185c29833a6b6fc44
Bryan <btteh@hotmail.com>
2018-02-03 05:17:30 +0800

    Made credits menu

---
+++ CyberCrack/Assets/Assets/Scripts/MainMenu/Credits.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Credits : MonoBehaviour
+{
+    MainMenuController mainMenuController;
+    TextMeshProUGUI exitText;
+    bool firstStart = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        mainMenuController = transform.parent.GetComponent<MainMenuController>();
+        exitText = transform.GetChild(2).GetComponent<TextMeshProUGUI>();
+
+        exitText.text = "Press " + GameData.instance.interact.ToString() + " to return to menu";
+        firstStart = false;
+    }
+
+    private void OnEnable()
+    {
+        if (!firstStart)
+            exitText.text = "Press " + GameData.instance.interact.ToString() + " to return to menu";
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetKeyDown(GameData.instance.interact))
+        {
+            mainMenuController.TogglePanel(false, MainMenuController.MainMenuPanels.Credits);
+        }
+    }
+}
